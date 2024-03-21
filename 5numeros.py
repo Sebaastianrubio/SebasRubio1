c9914e17b808e7d359fb12f9a8bb83fb42478043
Sebaastianrubio <159621000+Sebaastianrubio@users.noreply.github.com>
2024-03-20 20:33:51 -0600

    Add files via upload

---
+++ 5numeros.py
@@ -0,0 +1,6 @@
+
+# Escribe un programa en Python que utilice un bucle while para imprimir los primeros 5 números naturales.
+x=1
+while x<=5:  # Tenemos la Condicion While para controlar
+     print("Los Numeros son:",x)  # Impirmimos el Numero
+     x+=1  # Aqui aumentamos en 1 
