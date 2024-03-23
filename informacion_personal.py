216cf9b4c0961a6f72f60f1cc64f34c326c36bee
Sebaastianrubio <as.rubiof@uea.edu.ec>
2024-03-23 07:45:21 -0600

    version inicial

---
+++ informacion_personal.py
@@ -0,0 +1,8 @@
+print("UNIVERSIDAD ESTATAL AMAZONICA")
+  # Diccionarios
+#asignamos un diccionario y agregamos clave_valor
+informacion_personal ={"Nombre":"Sebastian","edad":"27_años","Ciudad":"Cuenca","Profesion":"Guardia","telefono":"0997310296"}
+#eliminamos la clave "edad" del diccionario
+del(informacion_personal["edad"])
+#imprimir diccionario final
+print (informacion_personal)
