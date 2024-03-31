1ced6d637bf7d84a9e30ae3a9f17220a3bed2c3f
Sebaastianrubio <as.rubiof@uea.edu.ec>
2024-03-31 00:35:20 -0500

    version inicial

--- calcular_descuento.py
+++ calcular_descuento.py
@@ -1,19 +1,19 @@
 print("UNIVERSIDAD ESTATAL AMAZONICA")
 #calcualdora
 nombre=input("ingrese el nombre del cliente:")
-cantidad=input("ingresa la cantidad de celulares comprados:")
+cantidad=input("Inngresa la cantidad de celulares comprados:")
 valor=input(input("ingrese el costo del celular:"))
 
 #Desarrollo
-montoTotal= cantidad * valor
+montoTotal = cantidad * valor
 
 descuento = (montoTotal * 10) /100
 
-Total_Pago = montoTotal_descuento
+Total_Pago = montoTotal-descuento
 
 #Salidas
-print(nombre,'Usted compro',cantidad,'celulares')
-print('valor sin Descuento es:',montoTotal)
+print(nombre,'sebastian Usted compro',cantidad,'2 celulares')
+print('300 valor sin Descuento es:',montoTotal)
 print('Descuento del 10%:',descuento)
 print('Usted debe Pagar con descuento:',Total_Pago)
 
